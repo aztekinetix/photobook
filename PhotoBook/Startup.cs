39f217cd3e8dd0c6e09cf23cbab10ec2ed5b95b9
a470195 <a470195@MC0XV65C>
2017-01-16 12:15:02 -0600

    Add project files.

---
+++ PhotoBook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PhotoBook.Startup))]
+namespace PhotoBook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
